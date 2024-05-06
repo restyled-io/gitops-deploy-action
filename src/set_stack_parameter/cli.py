6e3adcd84ea6d2ff6abf71845aa3013403ae54e2
patrick brisbin <pbrisbin@gmail.com>
2024-05-06 13:02:33 -0400

    Finalize set_stack_parameter as a project

--- src/set_stack_parameter/cli.py
+++ src/set_stack_parameter/cli.py
@@ -1,16 +1,18 @@
-import sys
 import argparse
+import sys
 
 from . import main
 
-parser = argparse.ArgumentParser()
-parser.add_argument("path", metavar="PATH", help="Stack file to read/write")
-parser.add_argument("name", metavar="NAME", help="Parameter Name to set")
-parser.add_argument("value", metavar="VALUE", help="Paramater Value to set")
-args = parser.parse_args()
 
-try:
-    main(args.path, args.name, args.value)
-except Exception as ex:
-    print("{}".format(ex), file=sys.stderr)
-    sys.exit(1)
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", metavar="PATH", help="Stack file to read/write")
+    parser.add_argument("name", metavar="NAME", help="Parameter Name to set")
+    parser.add_argument("value", metavar="VALUE", help="Paramater Value to set")
+    args = parser.parse_args()
+
+    try:
+        main(args.path, args.name, args.value)
+    except Exception as ex:
+        print("{}".format(ex), file=sys.stderr)
+        sys.exit(1)
